1095c2479073762c19726989506a1edb9b279d68
LynnFly <2453095588@qq.com>
2024-05-17 22:33:21 +0800

    first commit

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# 判断是否安装了 FFmpeg
+if command -v ffmpeg &> /dev/null
+then
+    echo "FFmpeg has been installed."
+    exit 0
+fi
+
 if command -v yum &> /dev/null
 then
     yum install wget tar yasm -y
@@ -8,6 +15,10 @@ then
 else
     echo "Unknown package manager, please install wget, tar, yasm manually."
     exit 1
+fi
+
+if [ ! -f ffmpeg-4.4.1.tar.gz ]; then
+    wget https://raw.githubusercontent.com/imlinfly/ffmpeg-install-script/main/ffmpeg-4.4.1.tar.gz
 fi
 
 /usr/bin/tar -zxvf ffmpeg-4.4.1.tar.gz
